1af7dbdbdb35a0f6777961685643d0f90ec2f2b4
dlegarda <legarda8027@gmail.com>
2024-03-08 00:33:47 -0500

    se introduce nuevas funciones al formulario

--- programa.js
+++ programa.js
@@ -1,11 +1,28 @@
 
-url='datos.json'
 
+
+url='datos.json'
 fetch(url)
 .then(response => response.json())
 .then(data => {
     console.log(data);
+    llenarDatos(data);
 })
 .catch(error => console.error('Error:', error))
 
+function llenarDatos(datos){
+    for(elemento of datos){
+        let nodo = document.createElement("option");
+        let texto = document.createTextNode(elemento.nombre);
+        let lista=document.getElementById('estudiantes');
+        let cont=lista.childElementCount;
+        nodo.appendChild(texto);
+        lista.appendChild(nodo);
+        console.log(cont);
+    }
+}
+
+function estadistica(){
+    numero=resizeTo AudioNode;
+}
 
